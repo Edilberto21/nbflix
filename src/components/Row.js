482c66ffc013f81f138f075c77fd8c64d8a1c9cf
Edilberto21 <edilbertobetinho93@gmail.com>
2022-11-27 17:27:37 -0300

    Showing movies from each category

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,5 +1,39 @@
-import React from "react";
-
-export default function Row() {
-  return <div>Row</div>;
+import React, { useEffect } from "react";
+import { getMovies } from "../api";
+import "./Row.css";
+const imageHost = "https://image.tmdb.org/t/p/original/";
+function Row({ title, path }) {
+  const [movies, setMovies] = React.useState([]);
+  const fetchMovies = async (_path) => {
+    try {
+      const data = await getMovies(_path);
+      console.log("data", data);
+      setMovies(data?.results);
+    } catch (error) {
+      console.log("fetchMovies error: ", error);
+    }
+  };
+  useEffect(() => {
+    //faz alguma coisa
+    fetchMovies(path);
+  }, [path]);
+  return (
+    <div className="row-container">
+      <h2 className="row-header">{title}</h2>
+      <div className="row-cards">
+        {movies?.map((movie) => {
+          return (
+            <img
+              className="movie-card"
+              key={movies.id}
+              src={`${imageHost}${movie.poster_path}`}
+              alt={movie.name}
+            ></img>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
+
+export default Row;
